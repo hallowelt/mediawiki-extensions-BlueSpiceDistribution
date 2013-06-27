0ff494f55d7698b145b18b19188d05a9d50986f3
Robert Vogel <vogel.robert@googlemail.com>
2013-06-27 16:57:02 +0200

    Fixed setup file

---
+++ BlueSpiceDistribution.php
@@ -0,0 +1,15 @@
+<?php
+
+require_once( __DIR__."/AdvancedMeta/AdvancedMeta.php" );
+//require_once( __DIR__."/Calendar/Calendar.php" ); //Crashes on MW 1.21
+require_once( __DIR__."/CategoryTree/CategoryTree.php" );
+require_once( __DIR__."/DynamicPageList/DynamicPageList.php" );
+require_once( __DIR__."/EmbedVideo/EmbedVideo.php" );
+//require_once( __DIR__."/LDAPAuthentication/LDAPAuthentication.php" ); //Makes only sense when you actually have LDAP
+require_once( __DIR__."/LiquidThreads/LiquidThreads.php" );
+require_once( __DIR__."/Lockdown/Lockdown.php" );
+require_once( __DIR__."/Math/Math.php" );
+require_once( __DIR__."/Quiz/Quiz.php" );
+require_once( __DIR__."/RSS/RSS.php" );
+require_once( __DIR__."/RSS-Topdate/Rss.php" );
+require_once( __DIR__."/WikiCategoryTagCloud/WikiCategoryTagCloud.php" );
