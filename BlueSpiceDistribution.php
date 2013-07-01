86c794f455a70f823618f2530a3d5b7e23ab664f
Marc Reymann <reymann@hallowelt.biz>
2013-07-01 14:47:09 +0200

    Liquid Threads? Why?

--- BlueSpiceDistribution.php
+++ BlueSpiceDistribution.php
@@ -6,7 +6,7 @@ require_once( __DIR__."/CategoryTree/CategoryTree.php" );
 require_once( __DIR__."/DynamicPageList/DynamicPageList.php" );
 require_once( __DIR__."/EmbedVideo/EmbedVideo.php" );
 //require_once( __DIR__."/LDAPAuthentication/LDAPAuthentication.php" ); //Makes only sense when you actually have LDAP
-require_once( __DIR__."/LiquidThreads/LiquidThreads.php" );
+//require_once( __DIR__."/LiquidThreads/LiquidThreads.php" );
 require_once( __DIR__."/Lockdown/Lockdown.php" );
 require_once( __DIR__."/Math/Math.php" );
 require_once( __DIR__."/Quiz/Quiz.php" );
