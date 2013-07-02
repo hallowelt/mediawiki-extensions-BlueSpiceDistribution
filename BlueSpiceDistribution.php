26021758a6f27c78ff92eb8a216cd3fbb6e615d3
smuggli <muggli@hallowelt.biz>
2013-07-02 16:47:21 +0200

    * Updating distribution

--- BlueSpiceDistribution.php
+++ BlueSpiceDistribution.php
@@ -4,12 +4,14 @@
 //require_once( __DIR__."/Calendar/Calendar.php" ); //Crashes on MW 1.21
 require_once( __DIR__."/CategoryTree/CategoryTree.php" );
 require_once( __DIR__."/Cite/Cite.php" );
-require_once( __DIR__."/DynamicPageList/DynamicPageList.php" );
+require_once( __DIR__."/InputBox/InputBox.php" );
+require_once( __DIR__."/intersection/DynamicPageList.php" );
 //require_once( __DIR__."/EmbedVideo/EmbedVideo.php" );
 //require_once( __DIR__."/LDAPAuthentication/LDAPAuthentication.php" ); //Makes only sense when you actually have LDAP
 //require_once( __DIR__."/LiquidThreads/LiquidThreads.php" );
 require_once( __DIR__."/Lockdown/Lockdown.php" );
 require_once( __DIR__."/Math/Math.php" );
+require_once( __DIR__."/Poem/Poem.php" );
 require_once( __DIR__."/Quiz/Quiz.php" );
 require_once( __DIR__."/RSS/RSS.php" );
 require_once( __DIR__."/SyntaxHighlight_GeSHi/SyntaxHighlight_GeSHi.php" );
