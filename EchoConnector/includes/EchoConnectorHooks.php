34e20d534458d227fe953c673206f7b2e6e8d8f0
Robert Vogel <vogel@hallowelt.biz>
2016-02-16 15:06:23 +0100

    Fixed EchoConnector

--- EchoConnector/includes/EchoConnectorHooks.php
+++ EchoConnector/includes/EchoConnectorHooks.php
@@ -19,14 +19,16 @@ class EchoConnectorHooks {
 	 */
 	public static function onEchoGetDefaultNotifiedUsers( $event, &$users ) {
 		$aAffectedUsers = $event->getExtra( 'affected-users' , array() );
-		$aAffectedGroups = $event->getExtra( 'affected-users' , array() );
+		$aAffectedGroups = $event->getExtra( 'affected-groups' , array() );
 
 		//Step 1: resolve groups to user_ids
-		$dbr = wfGetDB( DB_SLAVE );
-		$res = $dbr->select( 'user_groups', 'ug_user', array( 'ug_group' => $aAffectedGroups ) );
-		foreach( $res as $row ) {
-			$aAffectedUsers[] = $row->ug_user; //Append id to list of users.
-			//If a user is already on the list he/she will be filtered out below
+		if( !empty( $aAffectedGroups ) ) {
+			$dbr = wfGetDB( DB_SLAVE );
+			$res = $dbr->select( 'user_groups', 'ug_user', array( 'ug_group' => $aAffectedGroups ) );
+			foreach( $res as $row ) {
+				$aAffectedUsers[] = $row->ug_user; //Append id to list of users.
+				//If a user is already on the list he/she will be filtered out below
+			}
 		}
 
 		//Step 2: normalize list of users
