4d8f4a3db905a03997673adeb42def5c60f342ed
pwirth <wirth@hallowelt.biz>
2016-05-31 12:18:38 +0200

    Replaced all "Hallo Welt! - Medienwerkstatt" and updated copyright statements in file headers
    * Auto-Replaced all "Hallo Welt! - Medienwerkstatt GmbH" with "Hallo Welt! GmbH"
    * Updated all @copyright statements in the file headers

--- EchoConnector/includes/EchoTextEmailDecorator.php
+++ EchoConnector/includes/EchoTextEmailDecorator.php
@@ -6,7 +6,7 @@
  *
  * @author     Patric Wirth <wirth@hallowelt.biz>
  * @package    BlueSpice_Distrubution
- * @copyright  Copyright (C) 2012 Hallo Welt! - Medienwerkstatt GmbH, All rights reserved.
+ * @copyright  Copyright (C) 2016 Hallo Welt! GmbH, All rights reserved.
  * @license    http://www.gnu.org/copyleft/gpl.html GNU Public License v2 or later
  * @filesource
  */
--- EchoConnector/includes/NotificationsFormatter.php
+++ EchoConnector/includes/NotificationsFormatter.php
@@ -7,7 +7,7 @@
  * @author     Stefan Widmann <widmann@hallowelt.biz>
  * @author     Patric Wirth <wirth@hallowelt.biz>
  * @package    BlueSpice_Distribution
- * @copyright  Copyright (C) 2016 Hallo Welt! - Medienwerkstatt GmbH, All rights reserved.
+ * @copyright  Copyright (C) 2016 Hallo Welt! GmbH, All rights reserved.
  * @license    http://www.gnu.org/copyleft/gpl.html GNU Public License v2 or later
  * @filesource
  */
