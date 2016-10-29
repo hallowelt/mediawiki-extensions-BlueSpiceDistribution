141fdf5d639fb74f73fbbb81cf5e80e056690dc4
Markus Glaser <mrglaser@users.noreply.github.com>
2016-10-29 12:01:28 +0200

    fixed repository path

--- build/dist/init_bsdistribution_from_git_http.bash
+++ build/dist/init_bsdistribution_from_git_http.bash
@@ -149,7 +149,7 @@ EditNotify=(
 )
 
 BlueSpiceEditNotifyConnector={
-	"https://gerrit.wikimedia.org/r/mediawiki/extensions/BlueSpiceEditNotifyConnector",
+	"https://gerrit.wikimedia.org/r/p/mediawiki/extensions/BlueSpiceEditNotifyConnector",
 	"master",
 	"BlueSpiceEditNotifyConnector"
 }
